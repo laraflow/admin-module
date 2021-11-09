92b7b6028a135c77792e537812f9c4962daa8120
Hafijul Islam <hafijul233@gmail.com>
2021-11-10 01:44:40 +0600

    user role seeder created

--- Database/Seeders/UserSeeder.php
+++ Database/Seeders/UserSeeder.php
@@ -1,35 +1,13 @@
 <?php
 
-namespace Modules\Core\Database\Seeders;
+namespace Modules\Admin\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Admin\Models\Rbac\User;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * @var RegisteredUserService
-     */
-    private $registeredUserService;
-
-    /**
-     * @var UserRepository
-     */
-    private $userRepository;
-
-
-    /**
-     * UserSeeder constructor.
-     * @param RegisteredUserService $registeredUserService
-     * @param UserRepository $userRepository
-     */
-    public function __construct(RegisteredUserService $registeredUserService,
-                                UserRepository        $userRepository)
-    {
-        $this->registeredUserService = $registeredUserService;
-        $this->userRepository = $userRepository;
-    }
-
     /**
      * Run the database seeds.
      *
@@ -43,26 +21,7 @@ class UserSeeder extends Seeder
         $eventDispatcher = User::getEventDispatcher();
         User::unsetEventDispatcher();
 
-        //Default User "Ami"
-
-        try {
-            $newUser = [
-                'name' => 'Mohammad Hafijul Islam',
-                'username' => 'hafijul233',
-                'email' => 'hafijul233@gmail.com',
-                'password' => '123456789'
-            ];
-
-            if ($newRegisteredUser = $this->registeredUserService->attemptRegistration($newUser)) {
-                $this->userRepository->setModel($newRegisteredUser);
-                $adminRole = Role::findByName('Super Administration');
-                $feedback = $this->userRepository->manageRoles([$adminRole->id]);
-            }
-            //TODO else part
-        } catch (\Exception $exception) {
-            \Log::error($exception->getMessage());
-        }
-
+        User::factory(10)->create();
         //Enable observer
         User::setEventDispatcher($eventDispatcher);
 
---
+++ Models/Rbac/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modules\Admin\Models\Rbac;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Admin\Database\Factories\Rbac\UserFactory;
+use Modules\Core\Models\User as CoreUser;
+
+class User extends CoreUser
+{
+    use HasFactory;
+
+    protected $fillable = [];
+
+
+    /**
+     * Set Custom Factory Location
+     *
+     */
+    protected static function newFactory()
+    {
+        return UserFactory::new();
+    }
+
+}
