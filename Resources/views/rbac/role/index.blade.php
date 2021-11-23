e183c2d44cd434b3add07555fe8b4f30bd280125
Hafijul Islam <hafijul233@gmail.com>
2021-11-24 03:59:35 +0600

    empty wrapper added

--- Resources/views/rbac/role/index.blade.php
+++ Resources/views/rbac/role/index.blade.php
@@ -37,63 +37,69 @@
 @section('content')
     <div class="container-fluid">
         <div class="card card-default">
-            <div class="card-body p-0">
-                {!! \Html::cardSearch('search', 'admin.roles.index', 'Search Role Name, Code, Guard, Status, etc.') !!}
-                <div class="table-responsive">
-                    <table class="table table-hover mb-0" id="role-table">
-                        <thead class="thead-light">
-                        <tr>
-                            <th class="align-middle">
-                                @sortablelink('id', '#')
-                            </th>
-                            <th>@sortablelink('name', 'Name')</th>
-                            <th class="text-center">@sortablelink('guard_name', 'Guard')</th>
-                            <th class="text-center">@sortablelink('permissions', 'Permissions')</th>
-                            <th class="text-center">@sortablelink('users', 'Users')</th>
-                            <th class="text-center">@sortablelink('enabled', 'Enabled')</th>
-                            <th class="text-center">@sortablelink('created_at', 'Created')</th>
-                            <th class="text-center">Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @forelse($roles as $index => $role)
-                            <tr  @if($role->deleted_at != null) class="table-danger" @endif>
-                                <td class="exclude-search align-middle">
-                                    {{ $role->id }}
-                                </td>
-                                <td class="text-left">
-                                    @if(auth()->user()->can('admin.roles.show') || in_array($role->id, auth()->user()->role_ids))
-                                        <a href="{{ route('admin.roles.show', $role->id) }}">
-                                            {{ $role->name }}
-                                        </a>
-                                    @else
-                                        {{ $role->name }}
-                                    @endif
-                                </td>
-                                <td class="text-center">{{ $role->guard_name }}</td>
-                                <td class="text-center">{{ $role->total_permissions }}</td>
-                                <td class="text-center">{{ $role->total_users }}</td>
-
-                                <td class="text-center exclude-search">
-                                    {!! \Html::enableToggle($role) !!}
-                                </td>
-                                <td class="text-center">{{ $role->created_at->format(config('app.datetime')) ?? '' }}</td>
-                                <td class="exclude-search pr-3 text-center align-middle">
-                                    {!! \Html::actionDropdown('admin.roles', $role->id, array_merge(['show', 'edit'], ($role->deleted_at == null) ? ['delete'] : ['restore'])) !!}
-                                </td>
-                            </tr>
-                        @empty
+            @if(!empty($roles))
+                <div class="card-body p-0">
+                    {!! \Html::cardSearch('search', 'admin.roles.index', 'Search Role Name, Code, Guard, Status, etc.') !!}
+                    <div class="table-responsive">
+                        <table class="table table-hover mb-0" id="role-table">
+                            <thead class="thead-light">
                             <tr>
-                                <td colspan="7" class="exclude-search text-center">No data to display</td>
+                                <th class="align-middle">
+                                    @sortablelink('id', '#')
+                                </th>
+                                <th>@sortablelink('name', 'Name')</th>
+                                <th class="text-center">@sortablelink('guard_name', 'Guard')</th>
+                                <th class="text-center">@sortablelink('permissions', 'Permissions')</th>
+                                <th class="text-center">@sortablelink('users', 'Users')</th>
+                                <th class="text-center">@sortablelink('enabled', 'Enabled')</th>
+                                <th class="text-center">@sortablelink('created_at', 'Created')</th>
+                                <th class="text-center">Actions</th>
                             </tr>
-                        @endforelse
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            @forelse($roles as $index => $role)
+                                <tr @if($role->deleted_at != null) class="table-danger" @endif>
+                                    <td class="exclude-search align-middle">
+                                        {{ $role->id }}
+                                    </td>
+                                    <td class="text-left">
+                                        @if(auth()->user()->can('admin.roles.show') || in_array($role->id, auth()->user()->role_ids))
+                                            <a href="{{ route('admin.roles.show', $role->id) }}">
+                                                {{ $role->name }}
+                                            </a>
+                                        @else
+                                            {{ $role->name }}
+                                        @endif
+                                    </td>
+                                    <td class="text-center">{{ $role->guard_name }}</td>
+                                    <td class="text-center">{{ $role->total_permissions }}</td>
+                                    <td class="text-center">{{ $role->total_users }}</td>
+
+                                    <td class="text-center exclude-search">
+                                        {!! \Html::enableToggle($role) !!}
+                                    </td>
+                                    <td class="text-center">{{ $role->created_at->format(config('app.datetime')) ?? '' }}</td>
+                                    <td class="exclude-search pr-3 text-center align-middle">
+                                        {!! \Html::actionDropdown('admin.roles', $role->id, array_merge(['show', 'edit'], ($role->deleted_at == null) ? ['delete'] : ['restore'])) !!}
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="7" class="exclude-search text-center">No data to display</td>
+                                </tr>
+                            @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-            <div class="card-footer bg-transparent pb-0">
-                {!! \Modules\Admin\Supports\CHTML::pagination($roles) !!}
-            </div>
+                <div class="card-footer bg-transparent pb-0">
+                    {!! \Modules\Admin\Supports\CHTML::pagination($roles) !!}
+                </div>
+            @else
+                <div class="card-body min-vh-100">
+
+                </div>
+            @endif
         </div>
     </div>
     <!-- /.container-fluid -->
